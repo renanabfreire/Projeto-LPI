6eaa402546f6c9ffdade1715020facc4ecf4a7b1
Fernanda <fernandadants@hotmail.com>
2024-03-12 16:06:45 -0300

    adicionando classe controller

---
+++ Project/include/Controller.h
@@ -0,0 +1,28 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Assessment.h"
+#include "Author.h"
+#include "Login.h"
+#include "Project.h"
+
+
+class Controller{
+
+    private:
+        std::vector <Project> projects;
+
+    public:
+        // Construtor
+        Controller();
+
+        // Destrutor
+        ~Controller();
+
+        // Métodos públicos
+        
+};
+
+#endif // CONTROLLER_H
